b31b809974f846bf96626499891d27e42a005345
Obinna Nwabia <valinevite@gmail.com>
2020-11-16 21:12:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 transport = {:railroads => {}}
-transport[:railroads][:pieces] = "4"
+transport[:railroads][:pieces] = 4
 transport[:railroads][:rent_in_dollars] = {}
 transport[:railroads][:names] = {}
 end
