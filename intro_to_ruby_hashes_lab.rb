482fa01c8c1f71f8b4077f623fea8086b2a646ae
Obinna Nwabia <valinevite@gmail.com>
2020-11-16 21:35:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,15 +24,15 @@ def monopoly_with_third_tier
   # complexity.
 transport = {:railroads => {}}
 transport[:railroads][:pieces] = 4
-transport[:railroads][:rent_in_dollars] = {:one_piece_owned => 25
-:two_pieces_owned => 50
-:three_pieces_owned => 100
+transport[:railroads][:rent_in_dollars] = {:one_piece_owned => 25,
+:two_pieces_owned => 50,
+:three_pieces_owned => 100,
 :four_pieces_owned => 200
 }
 transport[:railroads][:names] = {
-  :reading_railroad => {}
-  :pennsylvania_railroad => {}
-  :b_and_o_railroad => {}
+  :reading_railroad => {},
+  :pennsylvania_railroad => {},
+  :b_and_o_railroad => {},
   :shortline_railroad => {}
 }
 p transport
